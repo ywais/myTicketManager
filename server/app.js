1732c4865244b624bbfb2c550133a868d473e03f
ywais <yuvalwais@gmail.com>
2020-08-24 15:45:10 +0300

    add GET endpoint
    pass first backend test

--- server/app.js
+++ server/app.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const fs = require('fs').promises;
+// let filePath = process.env.NODE_ENV === 'test' ? './recordTest.json' : './record.json';
+let filePath = './data.json';
+
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static('../client/build'));
+
+app.get('/api/tickets', async (req, res) => {
+    let tickets = await fs.readFile(filePath);
+    let ticketsUnJSONed = JSON.parse(tickets);
+    res.send(ticketsUnJSONed);
+});
 
 module.exports = app;
