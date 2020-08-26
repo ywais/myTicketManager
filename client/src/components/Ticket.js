949414001d1acc0f507c458bf827bea36edf3af0
ywais <yuvalwais@gmail.com>
2020-08-26 11:57:58 +0300

    add ticket css, display date function

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -9,15 +9,35 @@ function Ticket(props) {
         }
     }
 
+    const displayDate = (creationTime) => {
+        let date = new Date(creationTime),
+            year = date.getFullYear(),
+            month = date.getMonth(),
+            day = date.getDate(),
+            hours = date.getHours(),
+            minutes = date.getMinutes(),
+            seconds = date.getSeconds();
+        if(month < 9) {
+            month = '0' + (month + 1);
+        } else {
+            if(month === 9) { month++ }
+        }
+        if(day < 10) { day = '0' + day; }
+        if(hours < 10) { hours = '0' + hours; }
+        if(minutes < 10) { minutes = '0' + minutes; }
+        if(seconds < 10) { seconds = '0' + seconds; }
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    }
+    
     return (
         <div className='ticket' id={props.id} style={props.style}>
             <button className="hideTicketButton" onClick={props.onClick}>hide</button>
-            <div className='ticketTitle'>{props.title}</div>
-            <div className='ticketContent'>{props.content}</div>
+            <h3 className='ticketTitle'>{props.title}</h3>
+            <p className='ticketContent'>{props.content}</p>
             <div className='ticketInfo'>
                 <div className='ticketUserInfo'>
                     <span className='ticketUserEmail'>{props.userEmail}</span>
-                    <span className='ticketCreationTime'>{props.creationTime}</span>
+                    <span className='ticketCreationTime'>{displayDate(props.creationTime)}</span>
                 </div>
                 <div className='ticketLabels'>{printLabels(props.labels)}</div>
             </div>
